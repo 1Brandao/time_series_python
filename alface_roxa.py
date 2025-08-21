d7d1e31e196e1bbfe098db6bea8047cf3636f847
1Brandao <guilhermebrandao789@gmail.com>
2025-08-21 19:49:40 -0300

    initial commit

---
+++ alface_roxa.py
@@ -0,0 +1,9 @@
+"""
+Nome do Script: Teste.py
+Descrição: Este script processa o arquivo CSV de dados de vendas,
+realiza a limpeza e gera um relatório consolidado.
+Autor: Guilherme Brandão
+Data: 2025-08-21
+Versão: 1.0
+Licença: MIT
+"""
